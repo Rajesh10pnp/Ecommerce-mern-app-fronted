88f1c83e32b28a559b7731b33fd0754f05cedfb4
Rajesh Deshwal <raju10.deshwal@gmail.com>
2026-06-02 20:51:23 +0530

    Updated Admin Route

--- src/components/Routes/AdminRoute.js
+++ src/components/Routes/AdminRoute.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useAuth } from "../../context/auth";
 import { Outlet } from "react-router-dom";
 import axios from "axios";
@@ -7,17 +7,22 @@ import { API } from "../../config";
 
 export default function PrivateRoute() {
   const [ok, setOk] = useState(false);
-  const [auth, setAuth] = useAuth();
+  const [auth] = useAuth();
 
   useEffect(() => {
     const authCheck = async () => {
-      const res = await axios.get(`${API}/api/v1/auth/admin-auth`);
-      if (res.data.ok) {
-        setOk(true);
-      } else {
+      try {
+        const res = await axios.get(`${API}/api/v1/auth/admin-auth`);
+        if (res.data.ok) {
+          setOk(true);
+        } else {
+          setOk(false);
+        }
+      } catch (error) {
         setOk(false);
       }
     };
+
     if (auth?.token) authCheck();
   }, [auth?.token]);
 

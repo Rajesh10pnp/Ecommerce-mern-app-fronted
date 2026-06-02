e9e6423db0821d72c855a929440b9e7280f57f32
Rajesh Deshwal <raju10.deshwal@gmail.com>
2026-06-02 20:59:47 +0530

    Updated Client code

--- src/components/Routes/AdminRoute.js
+++ src/components/Routes/AdminRoute.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useAuth } from "../../context/auth";
 import { Outlet } from "react-router-dom";
 import axios from "axios";
@@ -7,22 +7,17 @@ import { API } from "../../config";
 
 export default function PrivateRoute() {
   const [ok, setOk] = useState(false);
-  const [auth] = useAuth();
+  const [auth, setAuth] = useAuth();
 
   useEffect(() => {
     const authCheck = async () => {
-      try {
-        const res = await axios.get(`${API}/api/v1/auth/admin-auth`);
-        if (res.data.ok) {
-          setOk(true);
-        } else {
-          setOk(false);
-        }
-      } catch (error) {
+      const res = await axios.get(`${API}/api/v1/auth/admin-auth`);
+      if (res.data.ok) {
+        setOk(true);
+      } else {
         setOk(false);
       }
     };
-
     if (auth?.token) authCheck();
   }, [auth?.token]);
 
